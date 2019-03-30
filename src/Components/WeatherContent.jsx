a1924f0fdc3aeb3926ff4a000c75e824430fa28c
Oleh Kozhushko <olkozhushko@gmail.com>
2019-03-30 17:25:00 +0200

    add new div block

--- src/Components/WeatherContent.jsx
+++ src/Components/WeatherContent.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 
 const WeatherContent = ({data}) => {
+  const date = new Date();
+  const weekDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
   return (
     <div className="weather-box">
-      <p className="weather-box__location">
+      <div className="weather-box__location">
         <span className="city">{data.city},</span>
         <span className="country">{data.country}</span>
-      </p>
-      <p>
-        <span className="temp">{data.temp}</span>
+      </div>
+      <div className="weather-box__date">
+        <p>{weekDays[date.getDay()]}, {date.getDate()}</p>
+      </div>
+      <div className="weather-box__temp">
+        <span className="temp">{Math.round(data.temp)}</span>
         <span className="temp-sign">°C</span>
-      </p>
-      <p className="weather-box__condition">{data.condition}</p>
-      <div className="weather-box__status-icon">
-        <img src={data.weatherIcon} alt=""></img>
+      </div>
+      <div className="weather-box__condition">
+        <p>{data.condition}</p>
+        <img 
+          src={data.weatherIcon} 
+          alt="weather condition icon" 
+          className="status-icon"/>
       </div>
     </div>
   )
