25a690b799b4ab507fdff524f083c51cae71f8ef
Oleh Kozhushko <olkozhushko@gmail.com>
2019-03-30 12:28:08 +0200

    assign data from state to render

--- src/Components/WeatherContent.jsx
+++ src/Components/WeatherContent.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
-const WeatherContent = () => {
+const WeatherContent = ({data}) => {
   return (
     <div className="weather-box">
       <p className="weather-box__location">
-        <span className="city"></span>
-        <span className="country"></span>
+        <span className="city">{data.city},</span>
+        <span className="country">{data.country}</span>
       </p>
       <p>
-        <span className="temp">12.5</span>
+        <span className="temp">{data.temp}</span>
         <span className="temp-sign">°C</span>
       </p>
-      <p className="weather-box__condition">Clear</p>
+      <p className="weather-box__condition">{data.condition}</p>
       <div className="weather-box__status-icon">
+        <img src={data.weatherIcon} alt=""></img>
       </div>
     </div>
   )
